4d2b5a0784af111216accb878a681041626ff95b
Brianca Knight <briancaknight@gmail.com>
2024-03-22 13:55:28 -0700

    Initial Commit

---
+++ KidsEmotionQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KidsEmotionQuiz
+//
+//  Created by Brianca Knight on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KidsEmotionQuiz/KidsEmotionQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsEmotionQuizApp.swift
+//  KidsEmotionQuiz
+//
+//  Created by Brianca Knight on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct KidsEmotionQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
